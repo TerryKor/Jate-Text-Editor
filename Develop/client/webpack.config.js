2eead264d07733c4f52c14aca480eab53144bc42
Terry <misterfreemann@gmail.com>
2023-05-05 12:29:30 -0400

    minor bugs fixed now app listening on port 3000

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 // TODO: Add CSS loaders and babel to webpack.
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
@@ -24,9 +24,10 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
-      new MiniCssExtractPlugin(), //added css extract plugin
+
+      // new MiniCssExtractPlugin(), //added css extract plugin
       new InjectManifest({
-        swSrc: "./src/sw.js",
+        swSrc: "./src-sw.js",
         swDest: "service-worker.js",
       }),
       //added manifest file
@@ -53,7 +54,7 @@ module.exports = () => {
         //added rules
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: ['style-loader', "css-loader"],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
