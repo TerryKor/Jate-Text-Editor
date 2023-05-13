da0817ee676fae89bfad087fc229793cd542f64e
Terry <misterfreemann@gmail.com>
2023-05-13 16:57:49 -0400

    adding data to indexDB and showing header, bugs fixed

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,12 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log("PUT to the database");
-  const todosDb = await openDB("jate", 1);
-  const tx = todosDb.transaction("jate", "readwrite");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({id:1, content: content});
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
@@ -26,13 +26,17 @@ export const putDb = async (id, content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET all from the database");
-  const todosDb = await openDB("jate", 1);
-  const tx = todosDb.transaction("jate", "readonly");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log("result.value", result);
-  return result;
+
+  if (result){
+    return request ['content']
+  }
+
+  return '';
 };
 
 initdb();
